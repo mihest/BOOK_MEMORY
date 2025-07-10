6112004427bfd83e6a5363b2e905b4903d57d57b
Konstantin <57441046+HachBoB@users.noreply.github.com>
2025-07-10 16:15:16 +0400

    FPage

---
+++ src/pages/index.jsx
@@ -0,0 +1,15 @@
+const IndexPage = () => {
+
+    return (
+        <div className="bg-[url(/main.jpg)] h-[2160px] w-[3844px]">
+            <div className="inset-0 bg-black/50 w-full h-full flex justify-center">
+                <div className="flex flex-col items-center mt-[80px] space-y-[64px]">
+                    <img src="/main-ico.png" alt="main-ico"/>
+                    <span className="font-[Roboto-Slab] text-white text-[40px]/[100%] font-[400] text-thin">КАТАВ-ИВАНОВСКИЙ МУНИЦИПАЛЬНЫЙ РАЙОН</span>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default IndexPage;
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
 import Keyboard from "./pages/Keyboard.jsx";
+import IndexPage from "./pages/index.jsx";
 
 const AppRoutes = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/keyboard" element={<Keyboard />} />
-
+                <Route path="/" element={<IndexPage />} />
                 <Route path="*" element={<Navigate to="/" replace/>}/>
             </Routes>
         </Router>
